dba28a0e4bb085f281d79edd72eff5505b109f79
элина ахметханова <Lina>
2023-02-28 18:07:11 +0500

    1_2_HW

---
+++ Lesson_1/HW/1_2_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите ваши числa");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int c = int.Parse(Console.ReadLine()!);
+if(a > b&&a > c)
+{
+    Console.Write("Максимальное число " + $"{a}" );
+}
+else if(b > a&&b > c)
+{
+    Console.Write("Максимальное число " + $"{b} ");
+}
+else if(c > a&&c > b)
+{
+    Console.Write("Максимальное число " + $"{c} ");
+}
