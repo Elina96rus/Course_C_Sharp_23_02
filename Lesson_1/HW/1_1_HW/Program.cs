133ba457b3d55575163a4ae40ff233db80225e6b
элина ахметханова <Lina>
2023-02-28 13:42:43 +0500

    1_1_HW

---
+++ Lesson_1/HW/1_1_HW/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите ваши числa");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+if(a > b)
+{
+    Console.Write("Максимальное число " + $"{a}" + ". Минимальное число, " + $"{b} ");
+}
+else if(b > a)
+{
+    Console.Write("Максимальное число " + $"{b} " + ". Минимальное число " + $"{a} ");
+}
