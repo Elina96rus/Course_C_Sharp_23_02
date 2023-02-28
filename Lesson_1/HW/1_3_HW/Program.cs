35e8d5d4c846054cfff07828f1b05cb0ff5248e1
элина ахметханова <Lina>
2023-02-28 20:07:45 +0500

    1_3_HW

---
+++ Lesson_1/HW/1_3_HW/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите ваше число");
+int a = int.Parse(Console.ReadLine()!);
+if ((a % 2) == 0)
+{
+    Console.Write($"{a} " + "четное число");
+}
+else if((a % 2) != 0)
+{
+    Console.Write($"{a} " + "нечетное число");
+}
