78a98693af93d36fb34460e4b2e0d2216b78f93f
элина ахметханова <Lina>
2023-03-01 23:38:56 +0500

    Task 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine()!);
+
+void Div(int N){
+    if (N % 7 == 0 && N % 23 == 0)
+        Console.WriteLine("Кратно");
+    else
+        Console.WriteLine("Некратно");
+}
+Div(N);
