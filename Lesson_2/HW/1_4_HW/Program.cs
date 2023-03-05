72150bfe9a694450704507d8b5af7c3637a74da6
элина ахметханова <Lina>
2023-03-06 02:43:04 +0500

    1_4_HW

---
+++ Lesson_2/HW/1_4_HW/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет , является ли этот день выходным.
+
+void GetDayWeek(int num){
+    int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+    Console.WriteLine($"Введенное число: {num}");
+    if(numbers.Contains(num)){
+        if (num == 6 || num == 7){
+            Console.WriteLine($"{num} -> Выходной");
+        }else{
+            Console.WriteLine($"{num} -> Рабочий");
+        }
+    }else{
+        Console.WriteLine($"{num} -> Это не день недели");
+    }
+
+}
+
+GetDayWeek(int.Parse(Console.ReadLine()!));
