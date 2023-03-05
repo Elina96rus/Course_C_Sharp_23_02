158bd40ca150c1fe54072d50aad404c3f31727c2
элина ахметханова <Lina>
2023-03-06 02:20:37 +0500

    1_3_HW

---
+++ Lesson_2/HW/1_3_HW/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+void GetNumThree(int num){
+    Console.WriteLine($"Введенное число: {num}");
+    if(num < 100){
+        Console.WriteLine("Третьего числа нет!");
+        return;
+    }
+
+    while(num > 999){
+        num /= 10;
+    }
+    Console.WriteLine($"Третья цифра: " + num % 10);
+}
+
+GetNumThree(int.Parse(Console.ReadLine()!));
